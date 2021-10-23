c634e3ad2e0a4d3b66309d8586fe564a5b1052cc
Albert Osei <albertosei@Alberts-MacBook-Air.local>
2021-10-23 16:01:40 +0000

    add save-txt file

--- web-scrapping2/main.py
+++ web-scrapping2/main.py
@@ -1,18 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 
+print('Put some skills that you are not familiar with')
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill}')
+
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
-for job in jobs:
+for index, job in enumerate(jobs):
     published_date = job.find('span', class_='sim-posted').span.text
     if 'few' in published_date:
-        company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
+        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
         skills = job.find('span', class_='srp-skills').text.replace(' ', '')
-
-        print(f'''
-        Company Name: {company_name}
-        Required Skills: {skills}
-        ''')
-
-        print('')
+        more_info = job.header.h2.a['href']
+        if unfamiliar_skill not in skills:
+            with open(f'posts/{index}.txt', 'w') as f:
+                f.write(f"Skills: {skills.strip()} \n")
+                f.write(f"Company Name: {company_name.strip()} \n")
+                f.write(f"More info: {more_info} \n")
+            print(f'File saved: {index}')
